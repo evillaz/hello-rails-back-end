986c269f3957fbe5ed2612279dc80fa64a8b4fc1
evillaz <evillaz123@gmail.com>
2023-08-11 11:16:31 -0500

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,7 @@ module HelloRailsBackEnd
     config.api_only = true
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'http://localhost:3000'
+        origins '*'
         resource '/api/*', headers: :any, methods: [:get, :options]
       end
     end
