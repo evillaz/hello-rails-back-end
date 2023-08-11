4e9ae4b41b7582df25d8c5b03addcd92c7e39bca
evillaz <evillaz123@gmail.com>
2023-08-11 12:08:56 -0500

    correct linters

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,7 +1,6 @@
 class Api::MessagesController < ApplicationController
   def random_greeting
-    random_message = Message.order("RANDOM()").first
+    random_message = Message.order('RANDOM()').first
     render json: { greeting: random_message.message }
   end
 end
-
