d54a4f7f48bb8f25b30ac21d3e72c91edd8fc997
evillaz <evillaz123@gmail.com>
2023-08-11 10:55:30 -0500

    Create api endpoint

---
+++ app/controllers/api/messages_controller.rb
@@ -0,0 +1,7 @@
+class Api::MessagesController < ApplicationController
+  def random_greeting
+    random_message = Message.order("RANDOM()").first
+    render json: { greeting: random_message.message }
+  end
+end
+
